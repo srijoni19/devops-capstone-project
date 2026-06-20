1e02caeeebd10cc9177e33ec6ac12da49d072755
srijoni19 <srijonibanerji19@gmail.com>
2026-06-20 22:41:53 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -5,12 +5,15 @@ This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
 import sys
-from flask import Flask
+from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
+talisman = Talisman(app)
+CORS(app)
 app.config.from_object(config)
 
 # Import the routes After the Flask app is created

e942147b8179950bd3d999e01fb184119f374e62
srijoni19 <srijonibanerji19@gmail.com>
2026-06-21 11:15:22 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,16 +17,17 @@ from service.routes import app
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
+
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
+######################################################################
 #  T E S T   C A S E S
-
-
+######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
-  
+
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
@@ -40,45 +41,47 @@ class TestAccountService(TestCase):
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
-      
-      
+
     def setUp(self):
         """Runs before each test"""
-        db.session.query(Account).delete()  # clean up the last tests
+        db.session.query(Account).delete()
         db.session.commit()
-
         self.client = app.test_client()
 
-  
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
-      
-      
+
     def test_security_headers(self):
         response = self.client.get(
             "/",
             environ_overrides=HTTPS_ENVIRON
         )
+
         self.assertEqual(
             response.headers["X-Frame-Options"],
             "SAMEORIGIN"
         )
+
         self.assertEqual(
             response.headers["X-Content-Type-Options"],
             "nosniff"
         )
+
         self.assertIn(
             "default-src",
             response.headers["Content-Security-Policy"]
         )
+
         self.assertEqual(
             response.headers["Referrer-Policy"],
             "strict-origin-when-cross-origin"
         )
-      
+
+    ######################################################################
     #  H E L P E R   M E T H O D S
-  
+    ######################################################################
+
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -94,21 +97,23 @@ class TestAccountService(TestCase):
             account.id = new_account["id"]
             accounts.append(account)
         return accounts
-      
+
+    ######################################################################
     #  A C C O U N T   T E S T   C A S E S
-  
+    ######################################################################
+
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-      
+
     def test_health(self):
         """It should be healthy"""
         resp = self.client.get("/health")
         self.assertEqual(resp.status_code, 200)
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
-      
+
     def test_create_account(self):
         """It should Create a new Account"""
         account = AccountFactory()
@@ -118,22 +123,22 @@ class TestAccountService(TestCase):
             content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Make sure location header is set
+
         location = response.headers.get("Location", None)
         self.assertIsNotNone(location)
-        # Check the data is correct
+
         new_account = response.get_json()
         self.assertEqual(new_account["name"], account.name)
         self.assertEqual(new_account["email"], account.email)
         self.assertEqual(new_account["address"], account.address)
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
-      
+
     def test_bad_request(self):
         """It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-      
+
     def test_unsupported_media_type(self):
         """It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
@@ -142,14 +147,17 @@ class TestAccountService(TestCase):
             json=account.serialize(),
             content_type="test/html"
         )
-        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
+        )
 
-    # ADD YOUR TEST CASES HERE ...
     def test_cors_headers(self):
         response = self.client.get(
             "/",
             environ_overrides=HTTPS_ENVIRON
         )
+
         self.assertEqual(
             response.headers["Access-Control-Allow-Origin"],
             "*"
